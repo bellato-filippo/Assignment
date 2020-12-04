31d83ab45e30512011483b00e58562d98e755016
bellato-filippo <bellatofilippo@gmail.com>
2020-12-04 17:38:37 +0100

    Main ready for beta testing

--- Assignment.cpp
+++ Assignment.cpp
@@ -1,25 +1,7 @@
-#include<iostream>
-#include <vector>
-#include "LaserScannerDrive.h"
-#include <ctime>
+#include "LaserScannerDriver.h";
+
 
 int main() {
 
-	srand(time(NULL));
-	std::vector<double> dati(180);
-	for (int i = 0; i < 180; i++) { //creo un vector di prova
-		dati[i] = (double)rand() / 1000;
-	}
-
-	LaserScannerDrive oggetto(1);
-	oggetto.new_scan(dati);
-	std::vector<double> prova = oggetto.get_scan();
-
-	for (int i = 0; i < prova.size(); i++) {
-		std::cout << prova[i] << "\n";
-	}
-
-	//oggetto.clear_buffer();
 	return 0;
 }
-
